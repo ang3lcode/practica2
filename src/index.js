a2e5aa84a36f297c88d41e1526caabf576034104
angel melendez <angelmelen88@gmail.com>
2023-07-09 21:31:38 -0600

    configuracion de webpack.config y entorno

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// const archivoJson = "ruta/archivo.json"; // Ruta al archivo .json
+// const contenidoJson = obtenerContenidoJson(archivoJson); // Función para leer el contenido del archivo
+
+// const objetoJson = JSON.parse(contenidoJson);
+// console.log(objetoJson);
+
+console.log('aqui no es juego ')
